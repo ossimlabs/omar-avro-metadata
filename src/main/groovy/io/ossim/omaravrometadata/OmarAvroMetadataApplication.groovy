3a8f40f6ee0079cf22ea0380206365997a9e43ad
slallier <stephen.lallier@digitalglobe.com>
2017-07-11 13:17:41 -0400

    initial commit of app

---
+++ src/main/groovy/io/ossim/omaravrometadata/OmarAvroMetadataApplication.groovy
@@ -0,0 +1,12 @@
+package io.ossim.omaravrometadata
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class OmarAvroMetadataApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run OmarAvroMetadataApplication, args
+	}
+}
---
+++ src/test/groovy/io/ossim/omaravrometadata/OmarAvroMetadataApplicationTests.groovy
@@ -0,0 +1,16 @@
+package io.ossim.omaravrometadata
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class OmarAvroMetadataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
