2767b5e31c79b39a9d8ff062de5028d5a8f342de
slallier <stephen.lallier@digitalglobe.com>
2017-07-11 16:04:40 -0400

    added region property, updated AvroMetadata table name

--- src/main/groovy/io/ossim/omaravrometadata/AvroMetadata.groovy
+++ src/main/groovy/io/ossim/omaravrometadata/AvroMetadata.groovy
@@ -4,7 +4,7 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable
 
-@DynamoDBTable(tableName = "AvroMetadata")
+@DynamoDBTable(tableName = "avro-metadata")
 class AvroMetadata
 {
     private String imageId
